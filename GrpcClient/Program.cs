7990e4ee347dd5d19ed9e49292f816af136903ec
GioMoreira <sgiomoreira@gmail.com>
2022-08-03 17:24:02 -0300

    Customer server and client + stream

--- GrpcClient/Program.cs
+++ GrpcClient/Program.cs
@@ -1,7 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using Grpc.Core;
 using Grpc.Net.Client;
 using GrpcServer;
+using GrpcServer.Protos;
 using System;
 using System.Threading.Tasks;
 
@@ -11,15 +13,39 @@ namespace GrpcClient
     {
         static async Task Main(string[] args)
         {
-            var input = new HelloRequest { Name = "Giovanna" };
+            //var input = new HelloRequest { Name = "Giovanna" };
+            //var channel = GrpcChannel.ForAddress("https://localhost:7015");
+            //var client = new Greeter.GreeterClient(channel);
+
+            //var reply = await client.SayHelloAsync(input);
+
+            //Console.WriteLine(reply.Message);
+
             var channel = GrpcChannel.ForAddress("https://localhost:7015");
-            var client = new Greeter.GreeterClient(channel);
+            var customerClient = new Customer.CustomerClient(channel);
 
-            var reply = await client.SayHelloAsync(input);
+            var clientRequested = new CustomerLookupModel { UserId = 2 };
 
-            Console.WriteLine(reply.Message);
+            var customer = await customerClient.GetCustomerInfoAsync(clientRequested);
 
-            Console.ReadLine();
+            Console.WriteLine($"{customer.FirstName} {customer.LastName}");
+
+            Console.WriteLine();
+            Console.WriteLine("New Customer List");
+            Console.WriteLine();
+
+            using (var call = customerClient.GetNewCustomers(new NewCustomerRequest()))
+            {
+                while (await call.ResponseStream.MoveNext())
+                {
+                    var currentCustomer = call.ResponseStream.Current;
+
+                    Console.WriteLine($"{currentCustomer.FirstName} {currentCustomer.LastName}: {currentCustomer.EmailAddress}");
+
+                }
+
+                Console.ReadLine();
+            }
         }
     }
 }
---
+++ GrpcServer/Services/CustomersService.cs
@@ -0,0 +1,82 @@
+﻿using Grpc.Core;
+using GrpcServer.Protos;
+
+namespace GrpcServer.Services
+{
+    public class CustomersService : Customer.CustomerBase
+    {
+        private readonly ILogger<CustomersService> _logger;
+
+        public CustomersService(ILogger<CustomersService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<CustomerModel> GetCustomerInfo(CustomerLookupModel request, ServerCallContext context)
+        {
+            CustomerModel output = new CustomerModel();
+
+            if (request.UserId == 1)
+            {
+                output.FirstName = "Jamie";
+                output.LastName = "Smith";
+            }
+            else if (request.UserId == 2)
+            {
+                output.FirstName = "Jane";
+                output.LastName = "Doe";
+            }
+            else
+            {
+                output.FirstName = "Greg";
+                output.LastName = "Thomas";
+            }
+
+            return Task.FromResult(output);
+        }
+
+        public override async Task GetNewCustomers(
+            NewCustomerRequest request, 
+            IServerStreamWriter<CustomerModel> responseStream, 
+            ServerCallContext context)
+        {
+
+            List<CustomerModel> customers = new List<CustomerModel>
+            {
+                new CustomerModel()
+                {
+                    FirstName = "Giovanna",
+                    LastName = "Moreira",
+                    EmailAddress = "sgiomoreira@gmail.com",
+                    Age = 23,
+                    IsAlive = true
+                },
+
+                new CustomerModel()
+                {
+                    FirstName = "John",
+                    LastName = "Snow",
+                    EmailAddress = "js@gmail.com",
+                    Age = 19,
+                    IsAlive = false
+                },
+
+                new CustomerModel()
+                {
+                    FirstName = "Lana",
+                    LastName = "Velaryon",
+                    EmailAddress = "lv@gmail.com",
+                    Age = 27,
+                    IsAlive = true
+                }
+            };
+
+            foreach (var cust in customers)
+            {
+                await Task.Delay(1000);
+                await responseStream.WriteAsync(cust);
+            }
+
+        }
+    }
+}
